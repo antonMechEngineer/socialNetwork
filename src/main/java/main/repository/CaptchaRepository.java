c92d39aab1f7150928fa909090ef89a5176295cf
Александр <rialex@rambler.ru>
2022-11-11 14:55:13 +0300

    11 Nov restored

--- src/main/java/main/repository/CaptchaRepository.java
+++ src/main/java/main/repository/CaptchaRepository.java
@@ -2,8 +2,18 @@ package main.repository;
 
 import main.config.entities.Captcha;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CaptchaRepository extends JpaRepository<Captcha, Integer> {
+    @Query(value = "SELECT * FROM captcha_codes WHERE secret_code =:secretCode", nativeQuery = true)
+    Optional<Captcha> findCaptchaBySecretCode(@Param("secretCode") String secretCode);
+
+    @Query(value = "SELECT * FROM captcha_codes WHERE time < (NOW() - INTERVAL 1 HOUR)", nativeQuery = true)
+    List<Captcha> findOldCaptchas();
 }
