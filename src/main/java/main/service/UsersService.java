2a7d61e3f29a42856af46eed6d265e95e888ccfb
Александр <rialex@rambler.ru>
2022-11-14 15:41:27 +0300

    POJO myProfile created

--- src/main/java/main/service/UsersService.java
+++ src/main/java/main/service/UsersService.java
@@ -1,19 +1,11 @@
 package main.service;
 
 import lombok.AllArgsConstructor;
-import main.api.request.RegisterRq;
-import main.api.response.ComplexRs;
-import main.api.response.RegisterRs;
-import main.config.entities.Captcha;
-import main.config.entities.Person;
+
 import main.repository.CaptchaRepository;
-import main.repository.PersonRepository;
 import main.repository.PersonsRepository;
 import org.springframework.stereotype.Service;
 
-import java.sql.Timestamp;
-import java.util.Date;
-import java.util.Optional;
 
 @Service
 @AllArgsConstructor
