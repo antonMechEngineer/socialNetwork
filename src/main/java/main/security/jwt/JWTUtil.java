0e3ff7a54b92730f6786bfa4bbbe06740bc779ec
Dmitrii Abdiev <abdiev.dn@yandex.by>
2022-11-16 09:11:52 +0300

    fix: endpoints added but still commented and token lifetime increase on 60 minutes

--- src/main/java/main/security/jwt/JWTUtil.java
+++ src/main/java/main/security/jwt/JWTUtil.java
@@ -28,7 +28,7 @@ public class JWTUtil {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
                 .signWith(SignatureAlgorithm.HS256, secret).compact();
     }
 
