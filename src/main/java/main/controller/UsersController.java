a6c644db299482ae5cad0094d70207d159bb2c17
Александр <rialex@rambler.ru>
2022-11-16 14:14:32 +0300

    cloudinary inputs

--- src/main/java/main/controller/UsersController.java
+++ src/main/java/main/controller/UsersController.java
@@ -1,7 +1,7 @@
 package main.controller;
 
 import main.api.request.UserRq;
-import main.api.response.PostsListResponse;
+import main.api.response.CommonResponse;
 import main.api.response.UserRs;
 import main.model.entities.Post;
 import main.service.PostsService;
@@ -11,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -26,27 +28,24 @@ public class UsersController {
     }
 
     @GetMapping("/{id}/wall")
-    public ResponseEntity<PostsListResponse> getUsersPosts(
+    public ResponseEntity<CommonResponse<List<Post>>> getUsersPosts(
             @PathVariable long id,
             @RequestParam(name = "page", required = false, defaultValue = "${socialNetwork.default.page}") int page,
             @RequestParam(name = "size", required = false, defaultValue = "${socialNetwork.default.size}") int size) {
         Page<Post> postPage = postsService.getAllPostsByAuthor(page, size, usersService.getPersonById(id));
-        return ResponseEntity.status(HttpStatus.OK).body(new PostsListResponse(
-                "success",
-                System.currentTimeMillis(),
-                postPage.getTotalElements(),
-                page,
-                postPage.getContent(),
-                size,
-                ""
-        ));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.<List<Post>>builder()
+                .error("success")
+                .timestamp(System.currentTimeMillis())
+                .offset(page)
+                .perPage(size)
+                .total(postPage.getTotalElements())
+                .data(postPage.getContent())
+                .build());
     }
 
-    //@PreAuthorize("hasAuthority('user:write')")
     @PutMapping("/me")
     ResponseEntity<UserRs> updateMyData(@RequestBody UserRq userRq){return null;}
 
-    //@PreAuthorize("hasAuthority('user:write')")
     @DeleteMapping("/me")
     ResponseEntity<UserRs>deleteMyData(){return null;}
 }
