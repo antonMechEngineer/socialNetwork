ba9d7cee2c93c228eb88753dc1e30490f1cd5b9b
Yuri Kaleganov <yurkaleganov@yandex.ru>
2022-11-16 11:32:39 +0300

    feat: add login / logout

--- src/main/java/main/api/request/UserRq.java
+++ src/main/java/main/api/request/UserRq.java
@@ -9,7 +9,7 @@ public class UserRq {
     private String about;
     private String city;
     private String country;
-    private  String first_name;
+    private String first_name;
     private String last_name;
     private String birth_date;
     private String photo_id;
--- src/main/java/main/controller/CommentsController.java
+++ src/main/java/main/controller/CommentsController.java
@@ -1,90 +1,90 @@
 package main.controller;
 
+import lombok.RequiredArgsConstructor;
 import main.api.request.CommentRequest;
-import main.api.response.CommentsListResponse;
+import main.api.response.CommonResponse;
 import main.errors.NoPostEntityException;
-import main.model.entities.Post;
 import main.model.entities.Comment;
+import main.model.entities.Post;
 import main.service.CommentsService;
 import main.service.PostsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/posts/{id}/comments")
+@RequiredArgsConstructor
 public class CommentsController {
 
     private final PostsService postsService;
     private final CommentsService commentsService;
 
-    @Autowired
-    public CommentsController(PostsService postsService, CommentsService commentsService) {
-        this.postsService = postsService;
-        this.commentsService = commentsService;
-    }
-
     @PostMapping
-    public ResponseEntity<CommentsListResponse> createComment(@PathVariable long id, @RequestBody CommentRequest commentRequest) throws NoPostEntityException {
+    public ResponseEntity<CommonResponse<List<Comment>>> createComment(@PathVariable long id, @RequestBody CommentRequest commentRequest) throws NoPostEntityException {
         Post post = postsService.findPostById(id);
         commentsService.createComment(post, commentRequest);
         Page<Comment> commentsPage = commentsService.getAllComments(0, 20);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommentsListResponse(
-                "success",
-                LocalDateTime.now(),
-                commentsPage.getNumber(),
-                commentsPage.getSize(),
-                new ArrayList<>(commentsPage.getContent()),
-                ""
-        ));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.<List<Comment>>builder()
+                .error("success")
+                .timestamp(System.currentTimeMillis())
+                .offset(commentsPage.getNumber())
+                .perPage(commentsPage.getSize())
+                .total(commentsPage.getTotalElements())
+                .data(new ArrayList<>(commentsPage.getContent()))
+                .build()
+        );
     }
 
     @GetMapping
-    public ResponseEntity<CommentsListResponse> getComments(
+    public ResponseEntity<CommonResponse<List<Comment>>> getComments(
             @PathVariable long id,
             @RequestParam(name = "page", required = false, defaultValue = "${socialNetwork.default.page}") int page,
             @RequestParam(name = "size", required = false, defaultValue = "${socialNetwork.default.size}") int size) {
         Page<Comment> commentsPage = commentsService.getAllComments(page, size);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommentsListResponse(
-                "success",
-                LocalDateTime.now(),
-                commentsPage.getNumber(),
-                commentsPage.getSize(),
-                new ArrayList<>(commentsPage.getContent()),
-                ""
-        ));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.<List<Comment>>builder()
+                .error("success")
+                .timestamp(System.currentTimeMillis())
+                .offset(commentsPage.getNumber())
+                .perPage(commentsPage.getSize())
+                .total(commentsPage.getTotalElements())
+                .data(new ArrayList<>(commentsPage.getContent()))
+                .build()
+        );
     }
 
     @DeleteMapping("/{comment_id}")
-    public ResponseEntity<CommentsListResponse> deleteComment(@PathVariable long id, @PathVariable long comment_id) {
+    public ResponseEntity<CommonResponse<List<Comment>>> deleteComment(@PathVariable long id, @PathVariable long comment_id) {
         commentsService.deleteComment(comment_id);
         Page<Comment> commentsPage = commentsService.getAllComments(0, 20);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommentsListResponse(
-                "success",
-                LocalDateTime.now(),
-                commentsPage.getNumber(),
-                commentsPage.getSize(),
-                new ArrayList<>(commentsPage.getContent()),
-                ""
-        ));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.<List<Comment>>builder()
+                .error("success")
+                .timestamp(System.currentTimeMillis())
+                .offset(commentsPage.getNumber())
+                .perPage(commentsPage.getSize())
+                .total(commentsPage.getTotalElements())
+                .data(new ArrayList<>(commentsPage.getContent()))
+                .build()
+        );
     }
 
     @PutMapping("/{comment_id}")
-    public ResponseEntity<CommentsListResponse> editComment(@PathVariable long id, @PathVariable long comment_id) {
-        commentsService.deleteComment(comment_id);
+    public ResponseEntity<CommonResponse<List<Comment>>> editComment(
+            @PathVariable long id, @PathVariable long comment_id, @RequestBody CommentRequest commentRequest) {
+        commentsService.editComment(comment_id, commentRequest.getCommentText());
         Page<Comment> commentsPage = commentsService.getAllComments(0, 20);
-        return ResponseEntity.status(HttpStatus.OK).body(new CommentsListResponse(
-                "success",
-                LocalDateTime.now(),
-                commentsPage.getNumber(),
-                commentsPage.getSize(),
-                new ArrayList<>(commentsPage.getContent()),
-                ""
-        ));
+        return ResponseEntity.status(HttpStatus.OK).body(CommonResponse.<List<Comment>>builder()
+                .error("success")
+                .timestamp(System.currentTimeMillis())
+                .offset(commentsPage.getNumber())
+                .perPage(commentsPage.getSize())
+                .total(commentsPage.getTotalElements())
+                .data(new ArrayList<>(commentsPage.getContent()))
+                .build()
+        );
     }
 }
