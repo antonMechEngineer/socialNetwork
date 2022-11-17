d4d3dcdbd232a8981b4cb741aab621bcdaaba1a5
Dmitrii Abdiev <abdiev.dn@yandex.by>
2022-11-17 17:15:41 +0300

    fix: ExpiredJwtException involved

--- src/main/java/main/security/jwt/JWTRequestFilter.java
+++ src/main/java/main/security/jwt/JWTRequestFilter.java
@@ -1,5 +1,7 @@
 package main.security.jwt;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import main.service.AuthService;
@@ -32,12 +34,21 @@ public class JWTRequestFilter extends OncePerRequestFilter {
                 if (auth != null) {
                     SecurityContextHolder.getContext().setAuthentication(auth);
                 }
+                filterChain.doFilter(request, response);
             }
-            filterChain.doFilter(request, response);
         } catch (ExpiredJwtException ex) {
+            ErrorResponse errorResponse = new ErrorResponse(ex);
+
             response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-            SecurityContextHolder.getContext().getAuthentication().setAuthenticated(false);
-            response.getWriter().write("");
+            response.getWriter().write(convertObjectToJson(errorResponse));
         }
+    }
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
