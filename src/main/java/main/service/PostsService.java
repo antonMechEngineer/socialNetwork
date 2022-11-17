d115064636e8b32c2788c435b0a300f35bbb44b9
Yuri Kaleganov <yurkaleganov@yandex.ru>
2022-11-17 12:51:19 +0300

    feat: add NetworkPageRequest for pagination, remove nullable from post_id in comments

--- src/main/java/main/service/PostsService.java
+++ src/main/java/main/service/PostsService.java
@@ -9,7 +9,6 @@ import main.model.entities.Tag;
 import main.repository.PostsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -42,13 +41,13 @@ public class PostsService {
 
     public Page<Post> getAllPosts(int page, int size) {
         Logger.getLogger(this.getClass().getName()).info("getAllPosts with page " + page + " and size " + size);
-        Pageable pageable = PageRequest.of(page, size, Sort.by("time").descending());
+        Pageable pageable = NetworkPageRequest.of(page, size, Sort.by("time").descending());
         return postsRepository.findAll(pageable);
     }
 
     public Page<Post> getAllPostsByAuthor(int page, int size, Person postsAuthor) {
         Logger.getLogger(this.getClass().getName()).info("getAllPostsByAuthor by " + postsAuthor + " with page " + page + " and size " + size);
-        Pageable pageable = PageRequest.of(page, size);
+        Pageable pageable = NetworkPageRequest.of(page, size);
         return postsRepository.findPostsByAuthorOrderByTimeDesc(pageable, postsAuthor);
     }
 
