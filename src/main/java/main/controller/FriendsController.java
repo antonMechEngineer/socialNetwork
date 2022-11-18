68f111f3f71e02ba0d00ded0808c1941632df2b7
Stanislav Molchanov <razboiniki03@gmail.com>
2022-11-18 18:53:49 +0300

    fix: change response status in ExceptionHandlerController for PersonNotFoundException from 200 to 401 fix: remove constructor in FriendsController and FriendsRecommendationService, add @RequiredArgsConstructor fix: remove Principal from method signature in FriendsController, add SecurityContextHolder refactor: rename PersonNotFoundByEmailException to PersonNotFoundException

--- src/main/java/main/controller/FriendsController.java
+++ src/main/java/main/controller/FriendsController.java
@@ -1,38 +1,26 @@
 package main.controller;
 
+import lombok.RequiredArgsConstructor;
 import main.api.response.CommonResponse;
 import main.api.response.PersonResponse;
-import main.errors.PersonNotFoundByEmailException;
 import main.service.FriendsRecommendationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.security.Principal;
 import java.util.List;
 
 @Controller
 @RequestMapping("/api/v1/friends")
+@RequiredArgsConstructor
 public class FriendsController {
 
     private final FriendsRecommendationService friendsRecommendationService;
 
-    @Autowired
-    public FriendsController(FriendsRecommendationService friendsRecommendationService) {
-        this.friendsRecommendationService = friendsRecommendationService;
-    }
-
     @GetMapping("/recommendations")
     @ResponseBody
-    public ResponseEntity<CommonResponse<List<PersonResponse>>> getRecommendedFriends(Principal principal) throws PersonNotFoundByEmailException {
-        List<PersonResponse> personResponses = friendsRecommendationService.getFriendsRecommendation(principal);
-        return ResponseEntity.ok(CommonResponse.<List<PersonResponse>>builder()
-                .timestamp(System.currentTimeMillis())
-                .total((long) personResponses.size())
-                .data(personResponses)
-                .build());
+    public CommonResponse<List<PersonResponse>> getRecommendedFriends() {
+        return friendsRecommendationService.getFriendsRecommendation();
     }
 }
