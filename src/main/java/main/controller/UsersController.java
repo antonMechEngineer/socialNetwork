14e4280b400c4513952b4094c7218fac434759c0
Александр <rialex@rambler.ru>
2022-11-23 10:45:10 +0300

    fix situation 23.Nov

--- src/main/java/main/controller/UsersController.java
+++ src/main/java/main/controller/UsersController.java
@@ -1,26 +1,13 @@
 package main.controller;
 
 import lombok.RequiredArgsConstructor;
-import main.api.request.PostRequest;
 import main.api.request.UserRq;
-import main.api.response.CommonResponse;
-import main.api.response.PersonResponse;
 import main.api.response.UserRs;
-import main.errors.BadAuthorizationException;
-import main.errors.PersonNotFoundException;
 import main.service.PersonsService;
 import main.service.PostsService;
 import main.service.UsersService;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.security.Principal;
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
