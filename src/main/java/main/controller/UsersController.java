18033813f37c318d17820fa2a5e3aee1c6ccf8f8
Александр <rialex@rambler.ru>
2022-11-23 11:08:04 +0300

    fix User Controller

--- src/main/java/main/controller/UsersController.java
+++ src/main/java/main/controller/UsersController.java
@@ -1,13 +1,21 @@
 package main.controller;
 
 import lombok.RequiredArgsConstructor;
+import main.api.request.PostRequest;
 import main.api.request.UserRq;
+import main.api.response.CommonResponse;
+import main.api.response.PersonResponse;
+import main.api.response.PostResponse;
 import main.api.response.UserRs;
+import main.errors.PersonNotFoundException;
 import main.service.PersonsService;
 import main.service.PostsService;
 import main.service.UsersService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
@@ -17,6 +25,33 @@ public class UsersController {
     private final PersonsService personsService;
     private final UsersService usersService;
 
+    @GetMapping("/{id}")
+    public CommonResponse<PersonResponse> getUserById(@PathVariable long id) {
+        return personsService.getPersonDataById(id);
+    }
+
+    @GetMapping("/{id}/wall")
+    public CommonResponse<List<PostResponse>> getUsersPosts(
+            @PathVariable long id,
+            @RequestParam(name = "offset", required = false, defaultValue = "${socialNetwork.default.page}") int offset,
+            @RequestParam(name = "itemPerPage", required = false, defaultValue = "${socialNetwork.default.size}") int size) {
+
+        return postsService.getAllPostsByAuthor(offset, size, personsService.getPersonById(id));
+    }
+
+    @PostMapping("/{id}/wall")
+    public CommonResponse<PostResponse> createPost(
+            @PathVariable(name = "id") Long personId,
+            @RequestParam(name = "publish_date", required = false) Long publishingDate,
+            @RequestBody PostRequest postRequest) throws PersonNotFoundException {
+
+        return postsService.createPost(postRequest, personId, publishingDate);
+    }
+
+    @GetMapping("/me")
+    public CommonResponse<PersonResponse> getMyData() {
+        return personsService.getMyData();
+    }
 
 
 
