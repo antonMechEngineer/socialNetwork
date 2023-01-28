7389aab290e11916f2585dbef2be026f2c82096d
Yuri Kaleganov <yurkaleganov@yandex.ru>
2023-01-29 00:05:04 +0300

    fix: lazy initialization bug for messages notifications fixed

--- src/main/java/soialNetworkApp/kafka/MessagesKafkaConsumer.java
+++ src/main/java/soialNetworkApp/kafka/MessagesKafkaConsumer.java
@@ -2,7 +2,6 @@ package soialNetworkApp.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import soialNetworkApp.kafka.dto.MessageKafka;
@@ -13,24 +12,15 @@ import soialNetworkApp.repository.DialogsRepository;
 import soialNetworkApp.repository.MessagesRepository;
 import soialNetworkApp.service.NotificationsService;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
-
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MessagesKafkaConsumer {
 
-    @Value("${socialNetwork.timezone}")
-    private String timezone;
-
     private final MessagesRepository messagesRepository;
     private final DialogsRepository dialogsRepository;
     private final DialogMapper dialogMapper;
     private final NotificationsService notificationsService;
-    private final NotificationsKafkaProducer notificationsKafkaProducer;
-
 
     @KafkaListener(topics = "messages", autoStartup = "${listen.auto.start:true}")
     public void consume(MessageKafka messageKafka) {
@@ -40,11 +30,6 @@ public class MessagesKafkaConsumer {
         Dialog dialog = dialogsRepository.findById(messageKafka.getDialogId()).orElseThrow();
         dialog.setLastMessage(message);
         dialogsRepository.save(dialog);
-        if (message.getRecipient().getPersonSettings().getMessageNotification() &&
-                LocalDateTime.now(ZoneId.of(timezone)).minus(1, ChronoUnit.MINUTES)
-                        .isAfter(message.getRecipient().getLastOnlineTime())) {
-            notificationsKafkaProducer.sendMessage(message, message.getRecipient());
-            notificationsService.sendNotificationToTelegramBot(message, message.getRecipient());
-        }
+        notificationsService.handleMessageForNotification(message.getId());
     }
 }
